6ed666de5257d8793b2fe0e1e67a7d2dcdcef3cd
Vitalii <vitaliti.yurchenko@gmail.com>
2024-09-28 00:31:54 +0300

    Add converter kilometers to miles

--- src/app/Main.java
+++ src/app/Main.java
@@ -11,12 +11,33 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         double kilometers;
         double miles;
+        int choice;
 
-        System.out.print("***Конвертер миль*** \nВведіть кількість миль: ");
-        miles = scanner.nextDouble();
+        System.out.println("\n***Конвертер миль/кілометрів***");
+        System.out.print("\n1. Конвертація миль в кілометри;\n2. Конвертація кілометрів в милі." +
+                "\nОберіть функцію (1,2): ");
+        choice = scanner.nextInt();
 
-        kilometers = miles * CONV;
+        if (choice == 1) {
+            System.out.print("\n***Конвертація миль в кілометри***\nВведіть кількіть миль: ");
+            miles = scanner.nextDouble();
+            kilometers = convMilesToKilometers(miles);
+            System.out.printf("В %s милі(-ях) ~%.2f кілометрів.", miles, kilometers);
+        } else if (choice == 2) {
+            System.out.print("\n***Конвертація кілометрів в милі***\nВведіть кількіть кілометрів: ");
+            kilometers = scanner.nextDouble();
+            miles = convKilometersToMiles(kilometers);
+            System.out.printf("В %s кілометрі(-ах) ~%.2f миль.", kilometers, miles);
+        } else {
+            System.out.print("\nОбрано невірний варіант. Спробуйте ще раз ;)");
+        }
+    }
 
-        System.out.println("В " + miles + " милі(-ях) " + kilometers + " кілометрів.");
+    private static double convMilesToKilometers(double miles) {
+        return miles * Main.CONV;
+    }
+
+    private static double convKilometersToMiles(double meters) {
+        return meters / Main.CONV;
     }
 }
