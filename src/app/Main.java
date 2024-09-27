381e271b0334c9a439c6b74ffe68bbde34f438a5
Vitalii <vitaliti.yurchenko@gmail.com>
2024-09-28 00:19:18 +0300

    Add converter miles to kilometers

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,9 +1,22 @@
 package app;
 
+import java.util.Scanner;
+
 public class Main {
+
+    private static final double CONV = 1.609344;
 
     public static void main(String[] args) {
 
-        System.out.println("Конвертер миль/метрів");
+        Scanner scanner = new Scanner(System.in);
+        double kilometers;
+        double miles;
+
+        System.out.print("***Конвертер миль*** \nВведіть кількість миль: ");
+        miles = scanner.nextDouble();
+
+        kilometers = miles * CONV;
+
+        System.out.println("В " + miles + " милі(-ях) " + kilometers + " кілометрів.");
     }
 }
